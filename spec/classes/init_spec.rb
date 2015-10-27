a7b3f55a0d37b2e21b6a084f10dd9207b76e34e0
Gabriel M Schuyler <gabe@puppetlabs.com>
2015-10-27 16:56:50 -0400

    Add more tests to main class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -6,22 +6,42 @@ describe 'checked_service', :type => :class do
     let(:facts) { {
       :kernel => 'Linux',
     } }
-
-    it {
+    it do
       should contain_class('checked_service')
-    }
-
+      should contain_package('zabby').with( {
+        'ensure' => '0.1.2',
+        'provider' => 'pe_gem',
+      } )
+      should contain_file('check service script').with( {
+        'ensure' => 'file',
+        'mode'   => '0755',
+        'owner'  => 'root',
+        'group'  => 'root',
+        'path'   => '/tmp/zbx-query.rb',
+      } )
+      should contain_file('check service script').with_content(/zabby/)
+    end
   end
 
   describe 'When called with no parameters on Windows' do
     let(:facts) { {
       :kernel => 'windows',
     } }
-
-    it {
+    it do
       should contain_class('checked_service')
-    }
-
+      should contain_package('zabby').with( {
+        'ensure' => '0.1.2',
+        'provider' => 'pe_gem',
+      } )
+      should contain_file('check service script').with( {
+        'ensure' => 'file',
+        'mode'   => '0755',
+        'owner'  => 'Administrator',
+        'group'  => 'Administrators',
+        'path'   => 'C:\Windows\Temp/zbx-query.rb',
+      } )
+      should contain_file('check service script').with_content(/zabby/)
+    end
   end
 
 end
